6b5674cb373e1fad3f728bc1a7219c885480014a
ZJ <1904841294@qq.com>
2023-07-30 11:34:40 +0800

    提交代码

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+  //int a=14;
+
+int main()
+{
+
+  /*printf("%d\n",sizeof(int));
+	printf("%d\n",sizeof(short));
+	printf("%d\n",sizeof(long));
+	printf("%d\n",sizeof(char));
+	printf("%d\n",sizeof(float));
+	printf("%d\n",sizeof(double));
+	printf("%d\n",sizeof(unsigned int));*/
+
+
+  /*int a=-10;
+	unsigned int b=20;
+	char c='E';
+	float d=3.14;
+	printf("%d\n",a);
+	printf("%d\n",b);
+	printf("%c\n",c);
+	printf("%f\n",d);
+	double e=3.141596;
+	printf("%lf\n",e);*/
+
+  //int a=90;
+  //printf("%d\n",a);
+
+	int a=0;
+	int b=0;
+	int sum=0;
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("%d\n",sum);
+	return 0;
+}
