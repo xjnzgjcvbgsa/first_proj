8cfa16a07164b641cf8f890a5c80f25658522c3b
ZJ <1904841294@qq.com>
2023-07-30 19:01:07 +0800

    提交代码

--- main.c
+++ main.c
@@ -1,38 +1,31 @@
 #include <stdio.h>
+//#include "add.h"
 
-  //int a=14;
+// int a=20;
+// extern int a;
+
+#define SUM 200
 
 int main()
 {
-
-  /*printf("%d\n",sizeof(int));
-	printf("%d\n",sizeof(short));
-	printf("%d\n",sizeof(long));
-	printf("%d\n",sizeof(char));
-	printf("%d\n",sizeof(float));
-	printf("%d\n",sizeof(double));
-	printf("%d\n",sizeof(unsigned int));*/
-
-
-  /*int a=-10;
-	unsigned int b=20;
-	char c='E';
-	float d=3.14;
+	/*int b=30;
 	printf("%d\n",a);
+	{
+		int c=40;
+		printf("%d\n",a);
+		printf("%d\n",b);
+		printf("%d\n",c);
+	}
+	printf("%d\n",b);*/
+
+ // printf("%d",c); error
+
+ // printf("%d\n",a);
+
+	int a=SUM;
+	const int b=38;
+	// b=28; error
+    // SUM=120; error
 	printf("%d\n",b);
-	printf("%c\n",c);
-	printf("%f\n",d);
-	double e=3.141596;
-	printf("%lf\n",e);*/
 
-  //int a=90;
-  //printf("%d\n",a);
-
-	int a=0;
-	int b=0;
-	int sum=0;
-	scanf("%d%d",&a,&b);
-	sum=a+b;
-	printf("%d\n",sum);
-	return 0;
 }
