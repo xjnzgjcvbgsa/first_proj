50f9f4decc83741258669d7904d9d1d1997d754d
ZJ <1904841294@qq.com>
2023-08-01 20:21:07 +0800

    提交代码

---
+++ main_8.1_1.cpp
@@ -0,0 +1,87 @@
+#include <stdio.h>
+
+//typedef unsigned int u_int;
+
+//struct Stu
+//{
+//	char name[30];
+//	int age;
+//	float score;
+//};
+
+int main()
+{
+	/*int a=0;
+	int b=~a;
+	printf("%d\n",b);
+	return 0;*/
+
+	/*int a=5;
+	int b=a++;
+	int c;
+	printf("%d\n",a);
+	printf("%d\n",b);
+	c=++a;
+	printf("%d\n",c);
+	return 0;*/
+
+	/*int a=3.14;
+	int b=a;
+	int c=(int)a;
+	int d=(int)3.14;
+	printf("%d\n",b);
+	printf("%d\n",c);
+	printf("%d\n",d);
+	return 0;*/
+
+	/*int a=1;
+	int b=2;
+	int c=a||b;
+	int d=a&&b;
+	int e=a|b;
+	int f=a&b;
+	printf("%d\n",c);
+	printf("%d\n",d);
+	printf("%d\n",e);
+	printf("%d\n",f);
+	return 0;*/
+
+	/*int a=3;
+	int b=8;
+	int max=0;
+	max=a>b?a:b;
+	return 0;*/
+
+	/*int a=3;
+	int b=6;
+	int c=4;
+	int d=(a+b,a|c,a<<2);
+	printf("%d\n",d);
+	return 0;*/
+
+	/*u_int a=10;
+	printf("%d\n",a);
+	return 0;*/
+
+	/*int a=3;
+	int *p=NULL;
+	p=&a;
+	*p=39;
+	char ch='u';
+	char*pc=&ch;
+	*pc='r';
+	printf("%p\n",&a);
+	printf("%d\n",a);
+	printf("%d\n",*p);
+	printf("%c\n",*pc);
+	printf("%c\n",ch);
+	printf("%d\n",sizeof(char*));
+	return 0;*/
+
+	/*struct Stu s={"ÕÅÈı",12,89.5};
+	printf("1. %s  %d  %f\n",s.name,s.age,s.score);
+	struct Stu*ps=&s;
+	printf("2. %s  %d  %f\n",(*ps).name,(*ps).age,(*ps).score);
+	printf("3. %s  %d  %f\n",ps->name,ps->age,ps->score );
+	return 0;*/
+}
